6e9da84166f42a268930a9a4e19216a534400952
Vishal Sharma <80253671+Visha14Sharma@users.noreply.github.com>
2024-02-17 12:40:18 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,19 @@
+
+/*function sayMyname(){
+    console.log("v");
+    console.log("i");
+    console.log("s");
+    console.log("H");
+    console.log("a");
+    console.log("l");
+}
+sayMyname()*/
+
+
+function addTwoNumber(number1, number2){
+  
+    return number1 + number2
+}
+
+const result = addTwoNumber(3, 5)
+console.log("Result:",result);
