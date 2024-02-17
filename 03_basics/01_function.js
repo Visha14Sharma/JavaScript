071a3dbc45a1855376d3b317d01881be0b90d3fb
Vishal Sharma <80253671+Visha14Sharma@users.noreply.github.com>
2024-02-17 12:46:12 +0000

    funtion

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -12,8 +12,22 @@ sayMyname()*/
 
 function addTwoNumber(number1, number2){
   
-    return number1 + number2
+    //return number1 + number2
 }
 
-const result = addTwoNumber(3, 5)
-console.log("Result:",result);
+//const result = addTwoNumber(3, 5)
+//console.log("Result:",result);
+
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("please enter a user name");
+        return
+
+    }
+    return `${username}just logged in `
+
+}
+
+console.log(loginUserMessage("vishal"));
+
