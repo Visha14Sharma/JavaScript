8095937f142af787597fea2b01996fe63ab72b8b
Vishal Sharma <80253671+Visha14Sharma@users.noreply.github.com>
2024-02-18 10:36:00 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -19,7 +19,7 @@ function addTwoNumber(number1, number2){
 //console.log("Result:",result);
 
 
-function loginUserMessage(username){
+/*function loginUserMessage(username){
     if(username === undefined){
         console.log("please enter a user name");
         return
@@ -29,5 +29,33 @@ function loginUserMessage(username){
 
 }
 
-console.log(loginUserMessage("vishal"));
+console.log(loginUserMessage("vishal"));*/
+
+
+function calculateCartPrice(var1, var2, ...num1){
+    return num1
+}
+console.log(calculateCartPrice(200, 500, 200))
+
+
+const user ={
+    username: "vishal",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray =[200, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
 
