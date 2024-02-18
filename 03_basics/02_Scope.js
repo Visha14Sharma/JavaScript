13fed1701a3144b2efe6ac9943d7470db1d52c15
Vishal Sharma <80253671+Visha14Sharma@users.noreply.github.com>
2024-02-18 10:46:08 +0000

    scope

---
+++ 03_basics/02_Scope.js
@@ -0,0 +1,14 @@
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30   
+}
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
