6dc895cec1b46de624e30fce8bc35af4ebba2739
Vishal Sharma <80253671+Visha14Sharma@users.noreply.github.com>
2024-02-18 11:02:36 +0000

    Scope

--- 03_basics/02_Scope.js
+++ 03_basics/02_Scope.js
@@ -8,7 +8,43 @@ if(true){
 //console.log(a);
 //console.log(b);
 //console.log(c);
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+
+function one() {
+    const username = "vishal"
+
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+    two()
+}
+
+//one()
+if (true) {
+    const username = "vishal"
+    if (username === "vishal") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+   // console.log(website);
+}
+//console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++interesting+++++++++++++++++++++++++++++++++++++++++++++
+
+function addone(num) {
+    return num +1
     
 }
+console.log (addone(5))                       
+
+
+const addTwo = function(num) {
+    return num +2 
+}
+
+addTwo(5)
